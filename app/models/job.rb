bcabb2470be0aeddce1ae4b2b746963206a525e1
Lucas dos Santos <lucas.leite@castgroup.com.br>
2018-08-06 14:37:49 -0300

    Correção remote-ok

--- app/models/job.rb
+++ app/models/job.rb
@@ -5,13 +5,13 @@ class Job
     self.jobTitle = jobTitle
     self.companyLogoURL = companyLogoURL
     self.companyName = companyName.split(".").first
-    self.jobDescription = jobDescription.force_encoding("ISO-8859-1").encode("UTF-8")
+    self.jobDescription = jobDescription#.split('Apply for this Job').first
     self.applyURL = applyURL
     self.tags = tags
     self.source = source
   end
 
   def to_s
-    "#{self.jobTitle} - #{self.companyName}"
+    "#{self.jobTitle} - #{self.jobDescription}"
   end
 end
